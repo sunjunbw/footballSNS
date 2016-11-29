cbdb452c8f81538233e3a05eb4f2afe274f8f3c8
xiaoruzhao <xiaoruzhao@qq.com>
2016-11-29 16:42:05 +0800

    add a method-dataformat

--- src/scripts/utils/commonUtil.js
+++ src/scripts/utils/commonUtil.js
@@ -1,6 +1,9 @@
 var commonUtil = {
 	setFocus:function($el){
        $el.addClass("active").siblings().removeClass("active");
+	},
+	dataFormat:function(){
+		console.log("我是被editMain修改的");
 	}
 }
 
